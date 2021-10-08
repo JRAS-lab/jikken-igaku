df02131ec134988eb38af8e871fcd785dd9ba46d
JRAS-customer <info+github@jras.co.jp>
2021-10-08 17:03:58 +0900

    add seurat sample

---
+++ sample_Seurat.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+pbmc <- readRDS("./data/pbmc3k.rds")
+
+DimPlot(pbmc, reduction = "umap")
+#IL7R, CCR7	→ Naive CD4+ T
+#IL7R, S100A4	→ Memory CD4+
+
+FeaturePlot(pbmc, features = c("IL7R", "CCR7", "S100A4"))
+
+
+new.cluster.ids <- c("0", "1", "2", "3", "4", "5",
+                     "6", "7")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
